d3625ce2a76658e45ae4c8662cd691d7c238b9ad
cagomni <cagomni@oocl.com>
2023-08-17 22:24:39 +0800

    test: create a test case when executeComman is turn right,(0,0,E) as initial location.

--- src/test/java/com/afs/tdd/MarsRoverTest.java
+++ src/test/java/com/afs/tdd/MarsRoverTest.java
@@ -88,6 +88,20 @@ class MarsRoverTest {
         Assertions.assertEquals( 0 , currentLocation.getY());
         Assertions.assertEquals(Direction.WEST , currentLocation.getDirection());
     }
+    @Test
+    void should_change_to_location_0_0_S_when_executeCommand_given_0_0_East_and_command_TurnRight() {
+        // Given
+        Location initialLocation = new Location(0, 0, Direction.EAST);
+        Command givenCommand = Command.TURN_RIGHT;
+        MarsRover marsRover = new MarsRover(initialLocation);
+        // When
+        marsRover.executeCommand(givenCommand);
+        Location currentLocation = marsRover.getCurrentLocation();
+        // Then
+        Assertions.assertEquals(0 , currentLocation.getX());
+        Assertions.assertEquals( 0 , currentLocation.getY());
+        Assertions.assertEquals(Direction.SOUTH , currentLocation.getDirection());
+    }
 
 
 }
