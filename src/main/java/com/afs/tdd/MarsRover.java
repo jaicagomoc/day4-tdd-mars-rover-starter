70d71ad69e4b23d9a40f7fe1b4dfb982bcd84062
cagomni <cagomni@oocl.com>
2023-08-17 22:19:32 +0800

    feat: should_change_to_location_0_0_W_when_executeCommand_given_0_0_South_and_command_TurnRight

--- src/main/java/com/afs/tdd/MarsRover.java
+++ src/main/java/com/afs/tdd/MarsRover.java
@@ -30,9 +30,9 @@ public class MarsRover {
         }
         if(givenCommand == Command.TURN_RIGHT) {
             if(location.getDirection() == Direction.NORTH){location.setDirection(Direction.EAST);}
-//            if(location.getDirection() == Direction.SOUTH){location.setY(location.getY() - 1);}
-//            if(location.getDirection() == Direction.EAST){location.setX(location.getX() + 1);}
-//            if(location.getDirection() == Direction.WEST){location.setX(location.getX() - 1);}
+            if(location.getDirection() == Direction.SOUTH){location.setDirection(Direction.WEST);}
+//            if(location.getDirection() == Direction.EAST){location.setDirection(Direction.EAST);}
+//            if(location.getDirection() == Direction.WEST){location.setDirection(Direction.EAST);}
             extracted();
         }
 
